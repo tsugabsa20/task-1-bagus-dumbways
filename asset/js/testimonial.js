bd9eac0e688d7a80acc8a0b92d432faff6280119
tsugabsa20 <bagusbintoro96@gmail.com>
2024-01-11 20:41:53 +0700

    task-7 done

--- asset/js/testimonial.js
+++ asset/js/testimonial.js
@@ -18,7 +18,7 @@ class Testimonial {
         this.#image = val
     }
 
-    set name (val) {
+    set description (val) {
         this.#description = val
     }
 
@@ -33,7 +33,27 @@ class Testimonial {
     get description () {
         return this.#description
     }
-    
 
+    html () {
+        return `  <div class="testimonial">
+        <img src="${this.image}" class="image-testimonial"/>
+        <p class="description">${this.description}</p>
+        <p class="author">${this.name}</p>
+    </div>`
+    }
 
 }
+
+const testimonial1= new Testimonial ("John Doe", "https://media.istockphoto.com/id/1230749818/photo/portrait-of-smiling-male-owner-of-fashion-store-standing-in-front-of-clothing-display.jpg?s=612x612&w=0&k=20&c=xoWhF-hrQcbMEPDYncHiHF8HJX2YgmYt7T-KLCPZIpY=", "bagus bang")
+
+const testimonial2= new Testimonial ("Rizal Muk", "https://onemarketmedia.com/wp-content/uploads/2020/06/testimonial-video.jpg", "yok bang semangat")
+const testimonial3= new Testimonial ("Rizal Muk", "https://onemarketmedia.com/wp-content/uploads/2020/06/testimonial-video.jpg", "yok bang semangat")
+
+const testimonials = [testimonial1, testimonial2, testimonial3]
+let testimonilaHTML = ""
+
+for(let index = 0; index < testimonials.length; index++) {
+    testimonilaHTML += testimonials[index].html()
+}
+
+document.getElementById("testimoni").innerHTML = testimonilaHTML
